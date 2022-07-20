27e42920188a64a9311ba22c396ee968a3e5e0f9
Koda Stokes <kodastokes11@gmail.com>
2022-07-20 12:58:16 -0600

    added error handlers

--- src/app.js
+++ src/app.js
@@ -1,5 +1,15 @@
 if (process.env.USER) require("dotenv").config();
 const express = require("express");
+const cors = require("cors");
 const app = express();
+
+const errorHandler = require("./errors/errorHandler");
+const notFound = require("./errors/notFound");
+
+app.use(cors());
+
+
+app.use(notFound);
+app.use(errorHandler);
 
 module.exports = app;
