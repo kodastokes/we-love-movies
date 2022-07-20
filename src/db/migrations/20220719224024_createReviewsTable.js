ec018a522b56fde9e060907d2cbeb178de255df2
Koda Stokes <kodastokes11@gmail.com>
2022-07-19 23:10:44 -0600

    updated migration table creations

--- src/db/migrations/20220719224024_createReviewsTable.js
+++ src/db/migrations/20220719224024_createReviewsTable.js
@@ -1,19 +1,24 @@
-
-exports.up = function(knex) {
-    return knex.schema.createTable("reviews", (table) => {
-        table.increments("review_id").primary()
-        table.text("content")
-        table.integer("score")
-        table.foreign("critic_id")
-             .reference("critic_id")
-             .inTable("critics")
-        table.foreign("movie_id")
-             .reference("movie_id")
-             .inTable("movies")
-        table.timestamps(true, true);
-    })
+exports.up = function (knex) {
+  return knex.schema.createTable("reviews", (table) => {
+    table.increments("review_id").primary();
+    table.text("content");
+    table.integer("score");
+    table.integer("critic_id").unsigned().notNullable();
+    table
+      .foreign("critic_id")
+      .references("critic_id")
+      .inTable("critics")
+      .onDelete("cascade");
+    table.integer("movie_id").unsigned().notNullable();
+    table
+      .foreign("movie_id")
+      .references("movie_id")
+      .inTable("movies")
+      .onDelete("cascade");
+    table.timestamps(true, true);
+  });
 };
 
-exports.down = function(knex) {
-    return knex.schema.dropTable("reviews");
+exports.down = function (knex) {
+  return knex.schema.dropTable("reviews");
 };
